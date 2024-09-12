4d8a30dca55116ce324d60658a4f4453f6c0f5ec
Saurabh Shukla <saurabh.shukla@happiestminds.com>
2024-09-12 12:20:26 +0530

    create from in vue Js & submit get data in console

--- blog/src/components/Signup.vue
+++ blog/src/components/Signup.vue
@@ -1,10 +1,40 @@
 <template>
     <div>
         <h3>Create SingUp From</h3>
+        <input type="text" name="username" id="username" placeholder="Enter Your Name" v-model="singup.username"/> <br/><br/>
+        <input type="password" name="password" id="password" placeholder="Enter Your Password" v-model="singup.password"/> <br/><br/>
+        <p>Choose Hobbies</p>
+        <input type="checkbox" id="cricket" value="cricket" v-model="singup.hobbies"/> 
+        <label for="cricket">Cricket</label>
+        <input type="checkbox" id="football" value="football" v-model="singup.hobbies"/> 
+        <label for="cricket">FootBall</label><br/><br/>
+        <p>Select Gender</p>
+        <input type="radio" name="gender" id="male" value="male" v-model="singup.gender"/> 
+        <label for="male">Male</label>
+        <input type="radio" name="gender" id="female" value="female" v-model="singup.gender"/> 
+        <label for="female">Female</label>
+        <br/><br/>
+        <button @click="singupUser">Submit</button>
     </div>
 </template>
 <script>
 export default {
-    name:"SingUpPage"
+    name:"SingUpPage",
+    data(){
+        return{
+            singup : {
+                username:null,
+                password:null,
+                hobbies:[],
+                gender:null
+            }
+        }
+    },
+    methods:{
+        singupUser()
+        {
+            console.log(this.singup)
+        }
+    }
 }
 </script>

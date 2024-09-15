86d5898dc72f7c2fa188bc2aaac137c608234b8e
Saurabh Shukla <saurabh.shukla@happiestminds.com>
2024-09-15 22:06:46 +0530

    non props & props data in vue js

---
+++ blog/src/components/ChildProps.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- <div>
+        <p>John</p>
+        <p>Mohan</p>
+        <p>Vivek</p>
+        <p>{{ data }}</p>
+    </div> -->
+
+    <!--  if i remove div here then we can you attar form single tag value like wise -->
+    <p v-bind="$attrs">John</p>
+    <!--  if access non props data if parent div not i have then we can use attributes -->
+    <p>Mohan</p>
+    <p>Vivek</p>
+    <p>{{ data }}</p>
+</template>
+<script>
+export default {
+    name:"ChildProps",
+    props:{
+        data:String
+    }
+}
+</script>

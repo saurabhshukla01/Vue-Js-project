d82e9f2cc56277cdafd8f510eb1c5aa81701229a
Saurabh Shukla <saurabh.shukla@happiestminds.com>
2024-09-15 17:45:15 +0530

    router param in vue js with diff components

---
+++ blog/src/components/User.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>User View Page</h2>
+        <h4>User Id is: {{ userId }}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "userPage",
+    data() {
+        return {
+            userId: this.$route.params.id // Corrected from param to params
+        };
+    }
+}
+</script>

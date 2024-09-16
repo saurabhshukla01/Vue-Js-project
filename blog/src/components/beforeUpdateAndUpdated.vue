99573e8ee8857fd0daf5af712cae236d506a62b6
Saurabh Shukla <saurabh.shukla@happiestminds.com>
2024-09-16 16:26:40 +0530

    beforeupdate & update hook

---
+++ blog/src/components/beforeUpdateAndUpdated.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h4 ref="count">{{ counter }}</h4>
+        <button @click="counter=counter+1">Update Count</button>
+    </div>
+</template>
+<script>
+export default {
+    name:"beforeUpdateAndUpdated",
+    data(){
+        return{
+            counter:0,
+        }
+    },
+    beforeUpdate(){
+        console.log("beforeUpdate",this.$refs['count'].textContent)   // get data from UI
+    },
+    updated(){
+        console.log("Updated",this.$refs['count'].textContent)
+    }
+}
+</script>

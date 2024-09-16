61e327a5b0dad97e4f9c35339a7e6b1002c1b399
Saurabh Shukla <saurabh.shukla@happiestminds.com>
2024-09-16 11:28:32 +0530

    create watch component in vue

---
+++ blog/src/components/watchersComponents.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Watchers</h2>
+        <h4>{{ count }}</h4>
+        <button @click="count=count+1">+</button>
+        <button @click="count=count-1">-</button>
+    </div>
+</template>
+<script>
+export default {
+    name:"watchersComponents",
+    data(){
+        return {
+            count:0
+        }
+    },
+    watch:{
+        count(val,prev){    // have 2 value first value & old value
+            if(val>5 && val>prev){
+                // when this value goes up to 5 then its reset value 0
+                this.count=0;
+                alert("stop counting");
+            }
+        }
+    }
+}
+</script>

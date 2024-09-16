d931e4e35a197d41268923b255d48fbedce21144
Saurabh Shukla <saurabh.shukla@happiestminds.com>
2024-09-16 10:56:27 +0530

    computed property get in vue

---
+++ blog/src/components/computedComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h5>Computed Component</h5>
+        <!-- Using direct logic in the template (this can affect performance for complex calculations) -->
+        <p>Logic is Dollar to rupee conversation less discount :  {{ (Dollar * rupeeVal) - discount  }}</p>
+
+        <!-- Using computed property for better performance -->
+        <p> Computed Property Result not need to small Brackets: {{ getResult }}</p>
+
+        <!-- Using method property but computed property is better performance to method  -->
+        <p> Method Property Result need to small Brackets: {{ getResultMethod() }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "computedComponent",
+    data() {
+        return {
+            Dollar: 100,
+            rupeeVal: 83.90,
+            discount: 30,
+        };
+    },
+    methods: {
+        // method property to calculate the result
+        getResultMethod() {
+            return (this.Dollar * this.rupeeVal) - this.discount;
+        }
+    },
+    computed: {
+        // Computed property to calculate the result
+        getResult() {
+            return (this.Dollar * this.rupeeVal) - this.discount;
+        }
+    }
+}
+</script>
